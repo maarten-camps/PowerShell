101376cc2bdf54db22ecb9ac49bc289277069119
Maarten Camps <maarten@cloud-masters.nl>
2024-07-08 08:19:04 +0200

    new file:   MG Groups/AssignGroups.ps1 	modified:   MG Groups/Readme.md

---
+++ MG Groups/AssignGroups.ps1
@@ -0,0 +1,38 @@
+## 
+#
+#  Assign Groups
+# Author Maarten Camps
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# # 
+
+
+
+#Connect-MgGraph -Scopes "User.Read.All", "Group.Read.All"
+
+$users = Import-Csv C:\temp\users.csv   #'.\MG Groups\Users.csv'
+$LicenseGroupID = 
+$ItemInsightsGroupID = 
+$OfficeUpgradeGroupID = 
+
+foreach ($User in $users) {
+    $UPN = $User.UPN
+    $UserID = get-MGuser -UserId $UPN | Select ID 
+    $ID = $UserID.Id
+
+    # Assign user to License Group
+    New-MgGroupMember -GroupId $LicenseGroupID -DirectoryObjectId $ID
+
+    #Enable Item insights for the Specific User 
+
+    Remove-MgGroupMemberByRef -GroupId $ItemInsightsGroupID -DirectoryObjectId $ID
+
+    #Get User Device info
+
+    #Add device to Secrity Group
+    
+}
+   
+
+$UserID = get-MGuser -UserId maarten.camps@tennet.eu | Select Id
+$userID
--- MG Groups/Readme.md
+++ MG Groups/Readme.md
@@ -1,4 +1,7 @@
 # Graph API user Managent
 
--- Assign users to a Group, Find the users device and add this to another Group
+- Assign users to a Group, Find the users device and add this to another Group
 
+### Required Modules  
+- Microsoft.Graph.Groups
+- Microsoft.Graph.Users
